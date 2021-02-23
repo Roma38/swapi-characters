899185c6d46f533399e79077c03ee2aa6a88d5a0
Roma Kutischev <rukkola2@gmail.com>
2021-02-23 16:55:06 +0200

    login-page layout

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,8 +1,9 @@
 import { useDispatch } from "react-redux";
+import { Header, Segment, Grid, Divider, Icon } from "semantic-ui-react";
 
 import SocialButton from '../components/SocialButton';
 
-import { FACEBOOK_APP_ID } from "../constants";
+import { FACEBOOK_APP_ID, LINKEDIN_APP_ID } from "../constants";
 import { storeUserData } from "../redux/actions/user";
 
 
@@ -14,19 +15,40 @@ function LoginPage() {
   }
 
   const handleSocialLoginFailure = err => {
-    alert(JSON.stringify(err));
+    console.log(err);
+    alert('Oops, something went wrong :(');
   }
 
-  return <div className="login-page">
-    <SocialButton
-      provider='facebook'
-      appId={FACEBOOK_APP_ID}
-      onLoginSuccess={handleSocialLogin}
-      onLoginFailure={handleSocialLoginFailure}
-    >
-      Login with Facebook
-      </SocialButton>
-  </div>
+  return <Segment placeholder>
+    <Grid columns={2} stackable textAlign='center'>
+      <Divider vertical>Or</Divider>
+
+      <Grid.Row verticalAlign='middle'>
+        <Grid.Column>
+          <Header icon as={SocialButton}
+            provider='facebook'
+            appId={FACEBOOK_APP_ID}
+            onLoginSuccess={handleSocialLogin}
+            onLoginFailure={handleSocialLoginFailure}
+            className="login-btn"
+          >
+            <Icon name='facebook' color='blue' /> Facebook
+          </Header>
+        </Grid.Column>
+        <Grid.Column>
+          <Header icon as={SocialButton}
+            provider='linkedin'
+            appId={LINKEDIN_APP_ID}
+            onLoginSuccess={handleSocialLogin}
+            onLoginFailure={() => alert('Sorry, authorization via LinkedIn not implemented yet :(')}
+            className="login-btn"
+          >
+            <Icon name='linkedin' color='blue' /> LinkedIn
+          </Header>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  </Segment>
 }
 
 export default LoginPage;
